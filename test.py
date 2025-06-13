f381590604c1e42425e083e90d6333f7b3b60403
Pathum Chinthaka <pathum.chinthaka@intellimorph.ai>
2025-06-13 17:00:43 +0530

    python collections and touples

---
+++ test.py
@@ -0,0 +1,21 @@
+# user_input= int(input("Enter a value: "))
+# f=3.14
+# r=20
+# radious= 0
+# # user_input=10
+
+# if user_input < 0:
+#     print("value is not positive")
+# else:
+#     radious = 2 * f * r
+#     print("radious is", radious)
+
+
+hypotenuse = 0
+
+side_one_user_input_= int(input("Enter side one value: "))
+side_two_user_input_= int(input("Enter side two value: "))
+
+hypotenuse = (side_one_user_input_ ** 2 + side_two_user_input_ ** 2) ** 0.5
+
+print ("The hypotenuse is", hypotenuse)
